b19b9e028125241ac2a880361f391ca3de40cd7b
AlexVesely <veselyalex11@gmail.com>
2025-10-21 15:39:21 +0100

    Update seeding to have some default cards and some random cards

--- database/seeders/CardTableSeeder.php
+++ database/seeders/CardTableSeeder.php
@@ -13,45 +13,21 @@ class CardTableSeeder extends Seeder
      */
     public function run(): void
     {
-        // $a = new Card;
-        // $a->name = "Anger";
-        // $a->energy_cost = 0;
-        // $a->rarity = "Common";
-        // $a->type = "Attack";
-        // $a->class = "Ironclad";
-        // $a->save();
+        $a = new Card;
+        $a->name = "Anger";
+        $a->energy_cost = 0;
+        $a->rarity = 'Common';
+        $a->type = 'Attack';
+        $a->class = 'Ironclad';
+        $a->save();
 
-        // $b = new Card;
-        // $b->name = "Backstab";
-        // $b->energy_cost = 0;
-        // $b->rarity = "Uncommon";
-        // $b->type = "Attack";
-        // $b->class = "Silent";
-        // $b->save();
-
-        // $c = new Card;
-        // $c->name = "Defragment";
-        // $c->energy_cost = 1;
-        // $c->rarity = "Uncommon";
-        // $c->type = "Power";
-        // $c->class = "Defect";
-        // $c->save();
-
-        // $d = new Card;
-        // $d->name = "Meditate";
-        // $d->energy_cost = 1;
-        // $d->rarity = "Uncommon";
-        // $d->type = "Skill";
-        // $d->class = "Watcher";
-        // $d->save();
-
-        // $e = new Card;
-        // $e->name = "Hand of Greed";
-        // $e->energy_cost = 2;
-        // $e->rarity = "Rare";
-        // $e->type = "Attack";
-        // $e->class = "Colourless";
-        // $e->save();
+        $b = new Card;
+        $b->name = "Backstab";
+        $b->energy_cost = 0;
+        $b->rarity = 'Uncommon';
+        $b->type = 'Attack';
+        $b->class = 'Silent';
+        $b->save();
 
         Card::factory()->count(20)->create();
     }
