4ff08c43b5989200c8d16c33b4b98ca7758c457f
AlexVesely <veselyalex11@gmail.com>
2025-11-04 14:46:31 +0000

    Created a secret route to test authentication

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/secret', function () {
+    return "Haha this is a secret";
+})->middleware(['auth']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
